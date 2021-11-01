3a17a87d9d747d1aeff04f9315650ae1af73b2d6
Paul Deen <paul.deen@dutchworkz.nl>
2021-11-01 19:59:19 +0100

    Minor: Fixed SonarQube code smell by refactoring for loop to LINQ query

--- src/CsharpExpressionDumper.Core/Extensions/EnumerableExtensions.cs
+++ src/CsharpExpressionDumper.Core/Extensions/EnumerableExtensions.cs
@@ -1,22 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CsharpExpressionDumper.Core.Extensions
 {
     public static class EnumerableExtensions
     {
         public static bool ProcessUntilSuccess<T>(this IEnumerable<T> instance, Func<T, bool> processDelegate)
-        {
-            foreach (var item in instance)
-            {
-                if (processDelegate(item))
-                {
-                    return true;
-                }
-            }
-
-            return false;
-        }
+            => instance.Any(x => processDelegate.Invoke(x));
 
         public static TResult ProcessUntilSuccess<TInput, TResult>(this IEnumerable<TInput> instance, Func<TInput, TResult> processDelegate)
             where TResult : class?
