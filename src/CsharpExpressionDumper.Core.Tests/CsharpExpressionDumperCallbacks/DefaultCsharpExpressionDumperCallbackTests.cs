6f3e68db99a9bca096725b990c2a0e80a37599b5
Paul Deen <paul.deen@dutchworkz.nl>
2022-02-03 19:30:28 +0100

    Added missing unit test

---
+++ src/CsharpExpressionDumper.Core.Tests/CsharpExpressionDumperCallbacks/DefaultCsharpExpressionDumperCallbackTests.cs
@@ -0,0 +1,20 @@
+﻿namespace CsharpExpressionDumper.Core.Tests.CsharpExpressionDumperCallbacks;
+
+public class DefaultCsharpExpressionDumperCallbackTests
+{
+    [Fact]
+    public void AppendTypeName_Throws_When_No_TypeNameFormatter_Returns_A_NonEmpty_Result()
+    {
+        // Arrange
+        var sut = new DefaultCsharpExpressionDumperCallback(Enumerable.Empty<ICustomTypeHandler>(),
+                                                            Enumerable.Empty<ITypeNameFormatter>(),
+                                                            Enumerable.Empty<IConstructorResolver>(),
+                                                            Enumerable.Empty<IReadOnlyPropertyResolver>(),
+                                                            Enumerable.Empty<IObjectHandlerPropertyFilter>());
+
+        // Act & Assert
+        sut.Invoking(x => x.AppendTypeName(typeof(string)))
+           .Should().ThrowExactly<ArgumentException>()
+           .WithMessage("Typename of type [System.String] could not be formatted");
+    }
+}
